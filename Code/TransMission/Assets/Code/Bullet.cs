498d8ae60195b9985fe6d50f53f3bfd0ee9a57a8
weis0061 <weis0061@algonquinlive.com>
2018-01-26 21:54:16 -0500

    trying to fix svn issues

---
+++ Code/TransMission/Assets/Code/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Bullet : MonoBehaviour
+{
+    Vector2 velocity { get { return GetComponent<Rigidbody>().velocity.xy(); } set { GetComponent<Rigidbody>().velocity = value; } }
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Code/TransMission/Assets/Code/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BulletPool
+{
+    const uint MAXBULLETS = 1200;
+    static Bullet[] Pool = new Bullet[MAXBULLETS];
+    public static void Destroy(this Bullet bullet)
+    {
+        bullet.gameObject.SetActive(false);
+        bullet.transform.parent = null;
+    }
+}
+public static class Extensions {
+    public static Vector2 xy(this Vector3 xyz)
+    {
+        return new Vector2(xyz.x, xyz.y);
+    }
+}
