0109f0f04c3dccfc3954bbeaaaa21a8d6bd86cfb
weis0061 <weis0061@algonquinlive.com>
2018-01-28 14:17:32 -0500

    main menu added

---
+++ Code/TransMission/Assets/Code/Menu/PlayButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour {
+
+    void OnMouseUpAsButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
