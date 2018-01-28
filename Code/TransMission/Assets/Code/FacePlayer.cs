c17c465b5c646a350b8cdcb80b23b1234e30363b
weis0061 <weis0061@algonquinlive.com>
2018-01-28 11:29:42 -0500

    a script that faces the player

---
+++ Code/TransMission/Assets/Code/FacePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour
+{
+    float speed = 0f;
+    GameObject player;
+    void Start()
+    {
+        player = FindObjectOfType<Drone>().gameObject;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameState.isGameActive)
+        {
+            transform.eulerAngles.Set(0, 0, Vector2.Angle(transform.position,player.transform.position));
+        }
+    }
+}
