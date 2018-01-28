5630f5f3fa95f8407a744a7beb1ca32090920dc2
weis0061 <weis0061@algonquinlive.com>
2018-01-28 16:44:44 -0500

    added win state fade, fixed drone constraints again

---
+++ Code/TransMission/Assets/Code/WinTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinTrigger : MonoBehaviour,ITrigger {
+    public GUITexture _guiTexture;
+    bool didWin = false;
+    public void Trigger()
+    {
+        didWin = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (didWin) _guiTexture.color = new Color(_guiTexture.color.r, _guiTexture.color.g, _guiTexture.color.b, _guiTexture.color.a + Time.deltaTime /8f);
+        if (_guiTexture.color.a > 1f) SceneManager.LoadScene(2);
+	}
+}
